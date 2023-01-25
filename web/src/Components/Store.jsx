588c676315872cfddae330788949a5c062828a05
joel gutierrez ruiz <joelgutierrezruiz11@gmail.com>
2023-01-25 23:15:34 +0100

    Hemos cambiado a react

---
+++ web/src/Components/Store.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/Store.css";
+function Store (){
+    return(
+        <div className="main__store">
+            <ul className="store__list">
+                <li className="store__list-item">
+                    <p>Objeto 1</p>
+                </li>
+            </ul>
+        </div>
+    );
+} 
+
+export default Store;
