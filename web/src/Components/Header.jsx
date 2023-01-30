be1f8b71768f8732b05363fe765edeb41b274e21
joel gutierrez ruiz <joelgutierrezruiz11@gmail.com>
2023-01-30 20:47:35 +0100

    Holaa

---
+++ web/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Styles/Header.css"
+function Header (){
+    return(
+        <div className="main__header">
+
+        </div>
+    );
+}
+
+export default Header
--- web/src/Components/Store.jsx
+++ web/src/Components/Store.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../Styles/Store.css";
 function Store (){
+
+    const peticion = fetch("http://localhost:5000/bananitos",{
+        method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            })
+    })
+
     return(
         <div className="main__store">
             <ul className="store__list">
